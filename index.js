b59e94f9968d6480ca40470848acb87b4b5fc27a
Abdus <azad3652@gmail.com>
2018-03-16 19:39:49 +0530

    updated ui design

--- index.js
+++ index.js
@@ -1,71 +1,128 @@
-"use strict"
+"use strict" 
+
+// variables for LOOPING
+let i, j, k, l;
+
+// variable declearation 
+let table, tableRow, tableData;
+let dynamicTableMaker;
+let rowNum, colNum;
+let cellBackgroundColor, cellSize;
+let idNo = 0;
 
 
 
 
-// this shit is working, please don't touch it even by mistake 
+// function to  to create table 
+const makeTable = () => {
 
-// table building 
-let m = 10;
-let n = 10;
-let createDynamicTable = document.getElementById("submit-table");
-let bodyWidth = (n * 20) + 50;
-let k=0;
-let button = document.createElement("button");
-let lineBrake =  document.createElement("br");
-let colorPixel = document.getElementById("choose-color").value; 
+    // elements creation
+    table = document.createElement("table");
+    tableRow = document.createElement("tr");
+    tableData = document.createElement("td");
+    rowNum = document.getElementById("row-num");
+    colNum = document.getElementById("col-num");
+    dynamicTableMaker = document.getElementById("submit-table");
 
-
-
-// function to build table 
-const createTable = () => {
-    for (let i = 0; i < m; i++) {
-        for (let j = 0; j < n; j++) {
-            button = document.createElement("button");
-            button.setAttribute("id", "button" + [k]);    // added a class to button element for applying event listener
-            document.getElementById("dynamic-table").appendChild(button);
-            k++;
-        } 
-        button = document.createElement("button");
-        lineBrake =  document.createElement("br");
-        button.setAttribute("class", "button");       // added a class to button element for applying event listener
-        document.getElementById("dynamic-table").appendChild(button.appendChild(lineBrake));
+    // loop to generate tables 
+    for (let i=0; i<=rowNum.value; i++) {
+        for (let j=0; j<colNum.value;j++) {
+            tableRow.appendChild(tableData);
+            tableData = document.createElement("td");
+            tableData.setAttribute("id", idNo);
+            idNo++;
+        }
+        tableRow = document.createElement("tr");
+        table.appendChild(tableRow);
+        document.getElementById("dynamic-table").appendChild(table);
     }
+
+    // cell dimension 
+    cellSize = document.getElementById("cell-size");
+    let td = document.getElementsByTagName("td");
+        for (k=0; k<td.length; k++){
+            td[k].style.height = cellSize.value + "px";
+            td[k].style.minWidth = cellSize.value + "px";
+        }
 }
+makeTable();
 
-// for keeping the background color of submit button white else it will be red on click 
-document.getElementById("submit-table").addEventListener("click", function(){
-    document.getElementById("submit-table").style.backgroundColor = "white";
+
+// dynamic table maker
+dynamicTableMaker.addEventListener("click", () => {
+    if (rowNum.value <= 100 && colNum.value <= 100 && rowNum.value >=0 && colNum.value >= 0) {
+        document.getElementById("dynamic-table").removeChild(table);
+        makeTable();
+    } else {
+        document.getElementById("overcount-alert").style.display = "inline";
+        document.getElementById("overcount-alert").style.opacity = "1";
+        setTimeout(() => {
+            document.getElementById("overcount-alert").style.display = "none";
+            document.getElementById("overcount-alert").style.opacity = "0";
+        }, 2000);
+    }
+
+    // default cell sizing 
+        let td = document.getElementsByTagName("td");
+        for (k=0; k<td.length; k++){
+            td[k].style.height = cellSize.value + "px";
+            td[k].style.minWidth = cellSize.value + "px";
+        }
 });
 
-// event listener for color 
-document.getElementById("choose-color").addEventListener("input", function(){
-    colorPixel = document.getElementById("choose-color").value; 
-})
 
 
-// event listener for table coloring 
-document.addEventListener('click', function(e){
-    if(e.target.tagName=="BUTTON" && document.getElementById(e.target.id).style.backgroundColor == ""){
-        let buttonId = e.target.id;
-        document.getElementById(buttonId).style.backgroundColor = colorPixel;  
-    } else {
-        let buttonId = e.target.id;
-        document.getElementById(buttonId).style.backgroundColor = "";
+
+// change cell size dynamically 
+const dynamicCellSize = () => {
+    cellSize = document.getElementById("cell-size");
+    cellSize.addEventListener("input", () => {
+        cellSize = document.getElementById("cell-size"); 
+        let td = document.getElementsByTagName("td");
+        for (k=0; k<td.length; k++){
+            td[k].style.height = cellSize.value + "px";
+            td[k].style.minWidth = cellSize.value + "px";
+        }
+    });
+}
+dynamicCellSize();
+
+// color the cell on right click and removing context menu    
+document.addEventListener("contextmenu", (e) => {
+    if (e.target.tagName === "TD") {
+        let id = e.target.id;
+        document.getElementById(id).style.backgroundColor = "unset";
+        document.getElementById(id).style.border = "1px solid rgba(0, 0, 0, 0.137)";
+        e.preventDefault(); 
     }
 });
-// ================================================================
-createTable();
-createDynamicTable.addEventListener("click", function(){
-    if (document.getElementById("row-num").value <=100 && document.getElementById("col-num").value <= 100) {
-        document.getElementById("dynamic-table").innerHTML = "";
-        m = document.getElementById("row-num").value;
-        n = document.getElementById("col-num").value; 
-        createTable();
-        // set a minimum body width dynamically 
-        bodyWidth = (n * 20) + 50;
-        document.getElementsByTagName("body")[0].style.minWidth = bodyWidth + "px";
-    } else {
-        alert("Rows and Columns count should be less than 100");
+
+
+// to put color 
+document.addEventListener("click", (e) => {
+    if (e.target.tagName === "TD") {
+        cellBackgroundColor = document.getElementById("cell-background-color");
+        let id = e.target.id;
+        document.getElementById(id).style.backgroundColor = cellBackgroundColor.value;
+        document.getElementById(id).style.border = "1px solid " + cellBackgroundColor.value;
+        e.preventDefault();
+    }
+});
+
+
+// circle or ractangle 
+cellBackgroundColor = document.getElementById("cell-background-color");
+document.getElementById("ractangle-canvas").style.background = cellBackgroundColor.value;
+document.getElementById("circle-canvas").style.background = cellBackgroundColor.value;
+document.getElementById("circle-canvas").addEventListener("click", () => {
+    let td = document.getElementsByTagName("td");
+    for (k=0; k<td.length; k++){
+        td[k].style.borderRadius = "50%";
+    }
+});
+document.getElementById("ractangle-canvas").addEventListener("click", () => {
+    let td = document.getElementsByTagName("td");
+    for (k=0; k<td.length; k++){
+        td[k].style.borderRadius = "0%";
     }
 });
